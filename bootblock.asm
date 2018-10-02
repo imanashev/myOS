4c990adafd3a97aa9ca9bc1ced782b75548693f8
Ildar Manashev <ildar404@gmail.com>
2018-10-02 16:56:41 +0700

    lab-03: sector read

--- bootblock.asm
+++ bootblock.asm
@@ -7,21 +7,36 @@ main:
     mov ss, ax
     mov ds, ax
     mov es, ax
-    jmp 0x0:init_cs
-
-init_cs:
-    mov ax, 0xb800
-    mov es, ax
-    xor bx, bx
+    jmp 0x0:clear_screen
 
 clear_screen:
     mov ah, 0x00             ; graphics mode
     mov al, 0x03             ; text mode
     int 0x10
 
+read_disk:
+    xor ax, ax				 ; load buffer address
+    mov es, ax
+    mov ax, 0x7e0
+    mov es, ax
+    xor bx, bx
 
-    mov si, string           ; load string
-    mov ah, 0x0a             ; set color
+    mov ah, 02h              ; чтение сектора
+    mov dl, 80h              ; номер диска
+    mov dh, 00h              ; номер головки  (стороны диска)
+	mov ch, 00h              ; номер дорожки
+    mov cl, 02h              ; номер сектора
+    mov al, 01h              ; число секторов
+    int 13h
+
+    xor ax, ax				 ; load videomem address
+    mov es, ax
+    mov ax, 0xb800
+    mov es, ax
+    xor bx, bx
+
+    mov si, 0x7e00			 ; load buffer address
+    mov ah, 0x0a			 ; set color
 
 print_loop:
     mov al, [si]             ; load symbol
@@ -32,18 +47,15 @@ print_loop:
     inc si                   ; next symbol
     jmp print_loop
 
+
+
 self: 
     jmp self
 
 ;------------------------
 
-string: 
-    db 'Hello, world!',0
-   ;  db '\
-   ;  _     ____  _     _     ___            _       ___   ___   _     ___   __   \
-   ; | |_| | |_  | |   | |   / / \  __      \ \    // / \ | |_) | |   | | \ |_/   \
-   ; |_| | |_|__ |_|__ |_|__ \_\_/ /_/)      \_\/\/ \_\_/ |_| \ |_|__ |_|_/ (_)   ',0
-
+; string: 
+;     db 'Hello, world!',0
 
 ;------------------------
 
