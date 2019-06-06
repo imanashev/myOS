14e37980cfa346bbb64fbf2fa9c7a7449ab97c81
imanashev <imanashev@gmail.com>
2019-06-06 14:27:55 +0700

    [+] add enter and tab processing

--- ports.cpp
+++ ports.cpp
@@ -8,6 +8,6 @@ void send_byte_to_port(u16 port, u8 data)
 u8 read_byte_from_port(u16 port)
 {
     u8 result;
-    asm volatile ("inb %1, %0": "=a"(result): "dN"(port) );
+    asm volatile ("inb %1, %0": "=a"(result): "dN"(port));
     return result;
 }
